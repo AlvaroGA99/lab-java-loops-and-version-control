f6af4d893090aef4e1b96b37ab8d35c8938e8c83
AlvaroGA99 <55707621+AlvaroGA99@users.noreply.github.com>
2025-04-01 17:38:34 +0200

    Second  Commit

--- ab-java-loops-and-version-control/src/main/java/Main.java
+++ ab-java-loops-and-version-control/src/main/java/Main.java
@@ -1,4 +1,54 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    private static final int x = 2;
+    private static final int y = 3;
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int minMaxDifference(int[] nums) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num < min) {
+                min = num;
+            }
+            if (num > max) {
+                max = num;
+            }
+        }
+
+        return max - min;
+    }
+
+    public static void printSmallestAndSecondSmallest(int[] arr) {
+        if (arr.length < 2) {
+            System.out.println("Array must contain at least two elements.");
+            return;
+        }
+
+        int smallest = Integer.MAX_VALUE;
+        int secondSmallest = Integer.MAX_VALUE;
+
+        for (int num : arr) {
+            if (num < smallest) {
+                secondSmallest = smallest;
+                smallest = num;
+            } else if (num < secondSmallest && num != smallest) {
+                secondSmallest = num;
+            }
+        }
+
+        if(secondSmallest == Integer.MAX_VALUE){
+            secondSmallest = smallest;
+        }
+        System.out.println("Smallest: " + smallest + ", Second Smallest: " + secondSmallest);
+
+    }
+
+    public static int expression(){
+        return x*x + (4*y/5 - x)*(4*y/5 - x);
+    }
 }
